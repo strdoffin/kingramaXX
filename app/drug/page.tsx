efb2e469257821cf33d6563ab73bdd118a422ba3
strdoffin <undefu5624@gmail.com>
2024-07-03 22:27:59 +0700

    Update page.tsx

--- app/drug/page.tsx
+++ app/drug/page.tsx
@@ -40,7 +40,7 @@ export default function page() {
       ];
   return (
     <div className='bg-fuchsia-700'>
-        <Link href={"/"} className='w-[50px] h-[50px] flex justify-center items-center text-4xl text-white text-center bg-yellow-400 rounded-full fixed top-5 left-5'><FontAwesomeIcon icon={faChevronLeft} /></Link>
+        <Link href={"/"} className='w-[50px] h-[50px] flex justify-center items-center text-4xl text-white text-center bg-yellow-400 rounded-full fixed top-5 left-5 z-10'><FontAwesomeIcon icon={faChevronLeft} /></Link>
         <nav className='text-5xl text-center py-2 text-yellow-300 font-bold'>นิทรรศการต้านยาเสพติดออนไลน์</nav>
         <Drug/>
         <div className='grid-cols-2 grid '>
